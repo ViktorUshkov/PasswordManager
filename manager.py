f8cdff2e1545ce24895bc0fad7757a5755090dc7
ViktorUshkov <mcderrigosh@yandex.ru>
2024-02-15 21:59:41 +0300

    first implementation of add/view passwords methods

--- manager.py
+++ manager.py
@@ -4,12 +4,15 @@
 экземпляра класса
 """
 
+import cryptography
+
 
 class PasswordManager:
     """ Класс, организующий работу с менеджером паролей """
 
     # разделитель между элементами одной записи в файле
     SEPARATOR = ' | '
+    KEY_FILE_PATH = 'key.key'
 
     def __init__(self, master_password: str, file: str):
         """
@@ -21,10 +24,32 @@ class PasswordManager:
         self.master_password = master_password
         self.file = file
 
-    def add_passwords(self) -> None:
-        pass
+    def add_passwords(self, service: str, login: str, password: str) -> None:
+        """
+        Функция, добавляющая запись, состоящую из названия интернет-сервиса (service), логина (login) и пароля (password)
+        в файл self.file
+
+        :param service: название интернет-сервиса, от учетной записи которого сохраняем пароль
+        :param login: логин от учетной записи
+        :param password: сохраняемый пароль
+        """
+        with open(self.file, 'a') as file:
+            file.write(service + self.SEPARATOR + login + self.SEPARATOR + password)
 
     def view_passwords(self) -> None:
+        """
+        Функция, выводящая на экран список паролей из файла self.file
+        """
+        with open(self.file, 'r') as file:
+            for line in file.readlines():
+                # составляющие части одной записи в файле - интернет-сервис, логин и пароль
+                service, login, password = line.rstrip().split(self.SEPARATOR)
+                print(f'{service}: логин - {login}, пароль - {password}')
+
+    def save_encrypt_key(self) -> None:
+        pass
+
+    def get_encrypt_key(self) -> str:
         pass
 
     def check_master_password(self) -> bool:
@@ -32,4 +57,3 @@ class PasswordManager:
 
     def interact(self) -> None:
         pass
-
